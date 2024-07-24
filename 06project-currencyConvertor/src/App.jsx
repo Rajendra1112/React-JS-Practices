7bffb839a7a547910390199fab218a8c567a9dd8
Rajendra-Thapa <thaparajendra1112@gmail.com>
2024-07-24 13:38:30 +0545

    setup of currencyConvertor project

---
+++ 06project-currencyConvertor/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="border-4 text-white bg-black m-5 border-orange-500 text-center">
+        hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
+
+// https://github.com/fawazahmed0/exchange-api?tab=readme-ov-file
+// https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json
---
+++ 06project-currencyConvertor/src/components/InputBox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InputBox({ label, className = "", currency }) {
+  return <div>inputbox</div>;
+}
+
+export default InputBox;
